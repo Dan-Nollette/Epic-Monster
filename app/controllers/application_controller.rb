778fb304e3a6d0d3154f7f33ae267e7c00d8af9b
Dan Nollette <nollette.dan@gmail.com>
2017-12-19 09:00:50 -0800

    add routes and MVC for users and sessions (authorization).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+  end
+
+  def current_admin
+    if (current_user.permission_level == 'admin')
+      @current_user = User.find(session[:user_id])
+    end
+  end
+
+  def authorize
+    if !current_user
+      flash[:alert] = "You aren't authorized to visit that page."
+      redirect_to '/'
+    end
+  end
+
+  def authorize_admin
+    if ( !current_admin )
+      flash[:alert] = "You aren't authorized to visit that page."
+      redirect_to '/'
+    end
+  end
 end
---
+++ db/migrate/20171219162428_add_users.rb
@@ -0,0 +1,11 @@
+class AddUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+    t.column :email, :string
+    t.column :first_name, :string
+    t.column :last_name, :string
+    t.column :permission_level, :string
+    t.column :password_hash, :string
+    t.column :password_salt, :string
+  end
+end
